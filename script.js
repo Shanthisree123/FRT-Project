de11e0d97eff468f049fbdcce269c7c14dac0042
Shanthisree123 <bellamshanthisree@gmail.com>
2022-10-01 21:53:38 +0530

    final project

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+
+<script>
+const d = new Date();
+document.getElementById("demo").innerHTML = d;
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
+</script>
